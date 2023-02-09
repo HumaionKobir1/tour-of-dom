70ad27dc202347d4c47023e003abd66496083da0
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-09 23:30:32 +0600

    practice of bynamic style, getAttribute, setAttribute, InnerText, innerHtml

--- dom.js
+++ dom.js
@@ -36,3 +36,32 @@ const someLi = document.querySelectorAll('.fruits-container li');
 for(const li of someLi){
     console.log(li.innerText);
 }
+
+
+// Dynamic style set use js
+const color = document.getElementById('fruits-title');
+color.style.backgroundColor = "purple";
+color.style.color = "white";
+
+
+
+// getAttribute
+const title = document.getElementById('fruits-title');
+// title.getAttribute('class');
+// title.classList;
+// title.classList.remove('blue-bg');
+// title.classList.add('purple-bg');
+
+// setAtribute
+// title.setAttribute('title', 'tootip set the javascript');
+
+
+// innerText
+const container = document.getElementsByClassName('fruits-container')[0].innerText;
+console.log(container);
+
+
+// innerHtml
+const container1 = document.getElementsByClassName('fruits-container')[0].innerHTML;
+console.log(container1);
+
