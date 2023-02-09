482f2496142320527520c65dc842aada20782406
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-09 08:33:26 +0600

    traversing dom - getElementByClass VS getElementById

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-// console.log('Hello from java script dom');
+console.log('Hello from java script dom');
 
 const liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
@@ -9,4 +9,12 @@ for(const li of liCollection){
 const allHeadings = document.getElementsByTagName('h1');
 for(const h1 of allHeadings){
     console.log(h1.innerText);
+}
+
+const fritsTitle = document.getElementById('fruits-title');
+fritsTitle.innerText = 'Frits changed by JS';
+
+const allImportantPlaces = document.getElementsByClassName('important-places');
+for(const places of allImportantPlaces){
+    console.log(places.innerText);
 }
